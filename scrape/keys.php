a3244dc40fe186b6127fb9ea8eef2d431607ce56
Lukas Joswiak <lukas@lukasjoswiak.com>
2016-12-18 18:32:35 -0800

    keys.php template

---
+++ scrape/keys.php
@@ -0,0 +1,7 @@
+<?php
+	// THESE KEYS MUST MATCH KEYS IN ./keys.py
+	$api_key = "PUT SECRET KEY HERE";
+	$update_key = "PUT SECRET KEY HERE";
+	$insert_key = "PUT SECRET KEY HERE";
+	$week_key = "PUT SECRET KEY HERE";
+?>
