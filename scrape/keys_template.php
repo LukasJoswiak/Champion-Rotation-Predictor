c725f480575c2337962bb6ca661452896dfdae3a
Lukas Joswiak <ljoswiak@cs.washington.edu>
2017-08-21 22:53:31 -0700

    Updated template to reflect updated requirements (API key no longer needed).

--- scrape/keys_template.php
+++ scrape/keys_template.php
@@ -2,7 +2,6 @@
 	// rename this file to keys.php
 	
 	// THESE KEYS MUST MATCH KEYS IN ./keys.py
-	$api_key = "PUT SECRET KEY HERE";
 	$update_key = "PUT SECRET KEY HERE";
 	$insert_key = "PUT SECRET KEY HERE";
 	$week_key = "PUT SECRET KEY HERE";
